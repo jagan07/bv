f9b8d2a92b55260f078123b3b4d0ee9735d1b35f
jagan07 <47105628+jagan07@users.noreply.github.com>
2019-02-27 16:14:58 +0530

    Create two numbers.c

---
+++ two numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,c,b,sum=0;
+printf("enter the limit");
+printf("\n enter the upto what u have to sum");
+scanf("%d%d",&c,&b);
+for(i=1;i<=c;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=b;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+getch();
+}
